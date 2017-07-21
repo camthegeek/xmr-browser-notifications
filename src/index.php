7223e7017a5ccc68959c04cb579963357ece387e
CamTheGeek <cam@camthegeek.net>
2017-07-21 11:47:20 -0400

    misc fixes
    bad firing of js, wrong stylesheet linked.

--- src/index.php
+++ src/index.php
@@ -6,16 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>CamTheGeek's Browser Block Notifier</title>
+    <title>CamTheGeek's XMR Browser Notifications</title>
     <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <link href="../calc/css/1.css" rel="stylesheet">
-    <script src="js/camthegeek.js"></script>
+   <link href="css/style.css" rel="stylesheet"> -->
     <script src="js/push.js"></script>
+    <script src="js/camthegeek.js"></script>
 	<style>
 		body {
 			background: #3d3d3d;
